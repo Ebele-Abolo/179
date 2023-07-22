c370f732d0411a8a8cd991cef067e688d87156d2
Ebele-Abolo <77888430+Ebele-Abolo@users.noreply.github.com>
2023-07-21 19:46:37 -0500

    Add files via upload

---
+++ 179.py
@@ -0,0 +1,6 @@
+if(input_value == self.color[self.random_number_for_color]):
+	self.__score = self.__score + random.randint(0,10)
+	obj.get_user_value(value)
+	input_value.delete(0,END)
+	btn - Button(root, text="CHECK" ,command=getInput, bg="IndianRed1", fg="white",
+		relief=FLAT, padx=10, pady=1, font=("Arial", 15))
